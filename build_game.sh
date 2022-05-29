d15fb7f98b68d0259710df88530baa54be2b4a15
vivax3794 <vivax3794@protonmail.com>
2022-05-29 12:00:45 +0200

    windows build

--- build_game.sh
+++ build_game.sh
@@ -1,6 +1,10 @@
 rm -rf builds
 mkdir builds
 mkdir builds/linux
+mkdir builds/windows
 
 cross build --target x86_64-unknown-linux-gnu --release
+cross build --target x86_64-pc-windows-gnu --release
+
 cp -v  target/x86_64-unknown-linux-gnu/release/astroids builds/linux
+cp -v  target/x86_64-pc-windows-gnu/release/astroids.exe builds/windows
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 use bevy;
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
