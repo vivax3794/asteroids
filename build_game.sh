7384ca2945624d6b55aebf31db0bd31f769c032e
vivax3794 <vivax3794@protonmail.com>
2022-05-29 11:20:07 +0200

    LETS USE A CROSS

--- build_game.sh
+++ build_game.sh
@@ -2,5 +2,5 @@ rm -rf builds
 mkdir builds
 mkdir builds/linux
 
-cargo build --target x86_64-unknown-linux-gnu --release
+cross build --target x86_64-unknown-linux-gnu --release
 cp -v  target/x86_64-unknown-linux-gnu/release/astroids builds/linux
